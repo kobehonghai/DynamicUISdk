46c72ce4922b67d127de3ceb439606d622d58a2c
liuhonghai <liuhonghai@qiyi.com>
2017-09-12 12:10:26 +0800

    ﻿新增relativelayoutlib

--- DynamicUIDemo/src/main/assets/listitem.lua
+++ DynamicUIDemo/src/main/assets/listitem.lua
@@ -3,31 +3,34 @@
 -- Date: 2017/9/8
 -- Time: 20:41
 --
-local mContext=context
-local layoutParams=relativeLayoutParams --need to do better
+require "LRelativeLayout"
 
+local mContext=context
 local Textview=luajava.bindClass('android.widget.TextView')
 local ImageView=luajava.bindClass("android.widget.ImageView")
-local RelativeLayout=luajava.bindClass("android.widget.RelativeLayout")
+
+local paramsLib=LRelativeLayout:newLayoutParams()
 
 local numText
 local icon
-local relativeLayout
 
 
 function getView()
     numText=Textview.new(mContext)
     icon=ImageView.new(mContext)
-    relativeLayout=RelativeLayout.new(mContext)
 
     numText:setId(1000)
     icon:setId(1001)
     icon:setBackgroundColor(0xffffff33)
-    relativeLayout:addView(numText)
 
-    layoutParams:addRule(RelativeLayout.RIGHT_OF,numText:getId())
-    relativeLayout:addView(icon,layoutParams)
-    return relativeLayout;
+    local textParam=paramsLib:getParams(paramsLib.WRAP_CONTENT,paramsLib.WRAP_CONTENT)
+    LRelativeLayout.addView(numText,textParam)
+    local iconParam=paramsLib:getParams(200,100,paramsLib.BELOW,numText:getId())
+    LRelativeLayout.addView(icon,iconParam)
+
+    LRelativeLayout.removeView(numText)
+
+    return LRelativeLayout.getContent()
 end
 
 function setData(data)
